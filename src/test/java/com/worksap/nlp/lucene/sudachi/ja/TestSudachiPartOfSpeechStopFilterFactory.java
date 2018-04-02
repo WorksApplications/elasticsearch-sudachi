898f829855eca58620df79ebfadf9f5c5f73c31f
takaoka_k <takaoka_k@worksap.co.jp>
2018-04-02 17:43:33 +0900

    Add tests

---
+++ src/test/java/com/worksap/nlp/lucene/sudachi/ja/TestSudachiPartOfSpeechStopFilterFactory.java
@@ -0,0 +1,73 @@
+/*
+ *  Copyright (c) 2018 Works Applications Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.worksap.nlp.lucene.sudachi.ja;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+import org.apache.lucene.analysis.BaseTokenStreamTestCase;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+
+public class TestSudachiPartOfSpeechStopFilterFactory extends BaseTokenStreamTestCase {
+    String path;
+    String settings;
+
+    @Rule
+    public TemporaryFolder tempFolderForDictionary = new TemporaryFolder();
+
+    public void setUp() throws Exception {
+        super.setUp();
+        tempFolderForDictionary.create();
+        File tempFileForDictionary = tempFolderForDictionary
+                .newFolder("sudachiDictionary");
+        ResourceUtil.copy(tempFileForDictionary);
+        path = tempFileForDictionary.getPath();
+
+        try (InputStream is = this.getClass().getResourceAsStream("sudachi.json")) {
+            settings = ResourceUtil.getSudachiSetting(is);
+        }
+    }
+
+    public void testBasics() throws IOException {
+        String tags = "動詞,非自立可能\n";
+        TokenStream ts = new SudachiTokenizer(true, SudachiTokenizer.Mode.NORMAL, path, settings);
+        ((Tokenizer)ts).setReader(new StringReader("東京都に行った。"));
+        Map<String, String> args = new HashMap<>();
+        args.put("tags", "stoptags.txt");
+        SudachiPartOfSpeechStopFilterFactory factory
+            = new SudachiPartOfSpeechStopFilterFactory(args);
+        factory.inform(new StringResourceLoader(tags));
+        ts = factory.create(ts);
+        assertTokenStreamContents(ts,
+                                  new String[] {"東京都", "に", "た"});
+    }
+
+    public void testBogusArguments() throws Exception {
+        IllegalArgumentException expected = expectThrows(IllegalArgumentException.class, () -> {
+                new SudachiPartOfSpeechStopFilterFactory(new HashMap<String, String>() {{ put("bogusArg", "bogusValue"); }});
+            });
+        assertTrue(expected.getMessage().contains("Unknown parameters"));
+    }
+}
