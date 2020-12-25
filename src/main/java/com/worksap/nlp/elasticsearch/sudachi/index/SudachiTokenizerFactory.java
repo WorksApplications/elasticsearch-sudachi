39e1bd85cafa0723fb8c13ac9b3cc22c0f0985f6
takaoka_k <takaoka_k@worksap.co.jp>
2020-12-25 15:33:07 +0900

    Fix compile error

--- src/main/java/com/worksap/nlp/elasticsearch/sudachi/index/SudachiTokenizerFactory.java
+++ src/main/java/com/worksap/nlp/elasticsearch/sudachi/index/SudachiTokenizerFactory.java
@@ -41,7 +41,7 @@ public class SudachiTokenizerFactory extends AbstractTokenizerFactory {
 
     public SudachiTokenizerFactory(IndexSettings indexSettings,
             Environment env, String name, Settings settings) throws IOException {
-        super(indexSettings, settings, name);
+        super(indexSettings, settings);
         mode = getMode(settings);
         discardPunctuation = settings.getAsBoolean("discard_punctuation", true);
         resourcesPath = getResourcesPath(env, settings);
