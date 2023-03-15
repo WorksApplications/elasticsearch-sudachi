80f6da104607e38132fdd4db802d82414aaf6844
Arseny Tolmachev <arseny@kotonoha.ws>
2023-03-16 08:59:22 +0900

    add basic framework for supporting OpenSearch

--- buildSrc/src/main/groovy/com/worksap/nlp/tools/es-sudachi.groovy
+++ buildSrc/src/main/groovy/com/worksap/nlp/tools/es-sudachi.groovy
@@ -2,83 +2,42 @@ package com.worksap.nlp.tools
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
+import org.gradle.api.provider.ProviderFactory
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
-enum EsSupport {
-    Es74("es-7.04"),
-    Es78("es-7.08"),
-    Es80("es-8.00"),
-    Es83("es-8.30"),
-    Es84("es-8.40")
+import javax.inject.Inject
 
-    String tag
-    List<String> keys
+class EsExtension {
+    Provider<? super EngineType> kind
 
-    EsSupport(String tag, String... keys) {
-        this.tag = tag
-        this.keys = Arrays.asList(keys)
+    @Inject
+    EsExtension(ProviderFactory providers) {
+        var engineVersion = providers.gradleProperty("engineVersion")
+        this.kind = engineVersion.map {new ProjectKind(it) }
     }
 }
-
-class Version {
-    int major
-    int minor
-
-    Version(int major, int minor) {
-        this.major = major
-        this.minor = minor
-    }
-
-    boolean ge(Integer major, Integer minor) {
-        if (major == this.major) {
-            return this.minor >= minor
-        }
-        return this.major >= major
-    }
-
-    boolean lt(Integer major, Integer minor) {
-        if (major == this.major) {
-            return this.minor < minor
-        }
-        return this.major < major
-    }
-
-    static EsSupport supportVersion(String version) {
-        var fields = version.split("\\.")
-        var major = fields[0].toInteger()
-        var minor = fields[1].toInteger()
-        var vers = new Version(major, minor)
-
-        if (vers.lt(7, 4)) {
-            throw new IllegalArgumentException("versions below 7.4 are not supported")
-        } else if (vers.ge(7, 4) && vers.lt(7, 8)) {
-            return EsSupport.Es74
-        } else if (vers.ge(7, 8) && vers.lt(8, 0)) {
-            return EsSupport.Es78
-        } else if (vers.ge(8, 0) && vers.lt(8, 3)) {
-            return EsSupport.Es80
-        } else if (vers.ge(8, 3) && vers.lt(8, 4)) {
-            return EsSupport.Es83
-        } else if (vers.ge(8, 4) && vers.lt(9, 0)) {
-            return EsSupport.Es84
-        } else {
-            throw new IllegalArgumentException("unsupported ElasticSearch version: " + version)
-        }
-    }
-}
-
 
 
 class EsSudachiPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        var verString = project.properties.get("elasticsearchVersion")
-        if (verString == null || !(verString instanceof String)) {
+        var rawVersion = project.property("engineVersion")
+        if (rawVersion == null || !(rawVersion instanceof String)) {
             throw new IllegalArgumentException("elasticVersion property is not defined")
         }
 
-        var version = Version.supportVersion(verString)
+        project.extensions.add("sudachiEs", EsExtension.class)
 
-        var tags = EsSupport.values().collectMany { v ->
+
+        var kind = new ProjectKind(rawVersion as String)
+        var verString = kind.version
+
+
+        var version = kind.supportVersion(verString)
+
+        var tags = kind.engine.allTags().collectMany { v ->
             var comparison = v <=> version
             if (comparison < 0) {
                 return List.of("${v.tag}-gt", "${v.tag}-ge")
@@ -89,9 +48,7 @@ class EsSudachiPlugin implements Plugin<Project> {
             }
         }
 
-        tags.addAll(version.keys)
-
-        println("ES support kind for version $verString is $version, using additional directories src/{test,main}/ext/{${tags.join(",")}}")
+        logger.warn("Compatibility for version $verString is $version, using additional directories src/{test,main}/ext/{${tags.join(",")}}")
 
         project.sourceSets {
             main.kotlin.srcDirs += tags.collect {"src/main/ext/$it" }
@@ -99,5 +56,24 @@ class EsSudachiPlugin implements Plugin<Project> {
             main.java.srcDirs += tags.collect {"src/main/ext/$it" }
             test.java.srcDirs += tags.collect {"src/test/ext/$it" }
         }
+
+
+        project.dependencies {
+            if (kind.engine == EngineType.ElasticSearch) {
+                compileOnly("org.elasticsearch:elasticsearch:$verString")
+                testImplementation("org.elasticsearch:elasticsearch:$verString")
+                testImplementation("org.elasticsearch.test:framework:$verString") {
+                    exclude(group: 'junit', module: 'junit')
+                }
+            } else {
+                compileOnly("org.opensearch:opensearch:$verString")
+                testImplementation("org.opensearch:opensearch:$verString")
+                testImplementation("org.opensearch.test:framework:$verString") {
+                    exclude(group: 'junit', module: 'junit')
+                }
+            }
+        }
     }
+
+    static final Logger logger = LoggerFactory.getLogger(EsSudachiPlugin.class)
 }
