0dd331253e354db42d841d655ffcaf72df2a682f
Hiyowa Kyobashi <kh.iyowa@gmail.com>
2025-08-13 16:22:08 +0900

    Upgrade Java version 11 to 17

--- buildSrc/src/main/groovy/com/worksap/nlp/tools/EsConventions.groovy
+++ buildSrc/src/main/groovy/com/worksap/nlp/tools/EsConventions.groovy
@@ -8,7 +8,7 @@ class EsConventions implements Plugin<Project> {
     @Override
     void apply(Project target) {
         target.tasks.withType(JavaCompile).configureEach {
-            options.release.set(11)
+            options.release.set(17)
             options.encoding = 'UTF-8'
         }
 
