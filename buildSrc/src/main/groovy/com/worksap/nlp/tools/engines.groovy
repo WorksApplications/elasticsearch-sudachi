066c9c8101a8e591698c966522270781611fe91c
ackintosh <sora.akatsuki@gmail.com>
2026-01-22 04:52:28 +0900

    Add os3 to EngineSupport

--- buildSrc/src/main/groovy/com/worksap/nlp/tools/engines.groovy
+++ buildSrc/src/main/groovy/com/worksap/nlp/tools/engines.groovy
@@ -51,6 +51,7 @@ enum OsSupport implements EngineSupport {
     Os20("os-2.00"),
     Os27("os-2.07"),
     Os210("os-2.10"),
+    Os30("os-3.00"),
 
     String tag
 
@@ -64,8 +65,10 @@ enum OsSupport implements EngineSupport {
             return Os20
         } else if (version.ge(2, 7) && version.lt(2, 10)) {
             return Os27
-        } else if (version.ge(2, 10)) {
+        } else if (version.ge(2, 10) && version.lt(3, 0)) {
             return Os210
+        } else if (version.ge(3, 0)) {
+            return Os30
         }
         throw new Exception("unsupported version")
     }
