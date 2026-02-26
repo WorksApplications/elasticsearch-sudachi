f64af9300e12511c768c632b1de0d689fd955d5c
hayashi-mas-wap <hayashi_mas+sudachi@worksap.co.jp>
2026-02-26 16:35:18 +0900

    fix aliase definition

--- search/src/test/ext/os-3/engine-aliases-test.kt
+++ search/src/test/ext/os-3/engine-aliases-test.kt
@@ -33,5 +33,3 @@ typealias NamedAnalyzer = org.opensearch.index.analysis.NamedAnalyzer
 typealias IndexAnalyzers = org.opensearch.index.analysis.IndexAnalyzers
 
 typealias XContentType = org.opensearch.common.xcontent.XContentType
-
-typealias Index = org.opensearch.core.index.Index
